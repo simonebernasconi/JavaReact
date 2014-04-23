1a025e4a07f35107e27068e4a1254f2318217a6e
simonebernasconi <simocomo90@hotmail.it>
2014-04-23 15:17:55 +0200

    Delete unused visitors and fix expressionhandler

--- JavaReact/src/test/javareact/examples/remote/ExampleDiv.java
+++ JavaReact/src/test/javareact/examples/remote/ExampleDiv.java
@@ -2,6 +2,7 @@ package test.javareact.examples.remote;
 
 import test.javareact.common.Consts;
 import test.javareact.common.packets.content.Value;
+import test.javareact.common.types.reactive.ReactiveBool;
 import test.javareact.common.types.reactive.ReactiveDouble;
 import test.javareact.common.types.reactive.ReactiveFactory;
 import test.javareact.common.types.reactive.ReactiveListener;
@@ -11,19 +12,19 @@ public class ExampleDiv implements ReactiveListener {
 	public static void main(String args[]) {
 		Consts.hostName = "ReactiveDiv";
 		ExampleDiv remoteReact = new ExampleDiv();
-		ReactiveDouble react= ReactiveFactory.getDouble("Remote1.obInt1.get() / Remote2.obInt2.get() ", 0, "obDiv");
+		ReactiveDouble react= ReactiveFactory.getDouble("Remote1.obInt1.get() < Remote2.obInt2.get() ", "obDiv");
 		react.addReactiveListener(remoteReact);
 	}
 
 	@Override
 	public void notifyReactiveChange(Value value) {
-		System.out.println("The reactive value is changed in: " + value.doubleVal());
+		System.out.println("The reactive value is changed in: " + value.boolVal());
 //		ObservableBool obAnd1 = new ObservableBool("obAnd1", false);
 //		obAnd1.set(value.boolVal());
 	}
 
 	@Override
 	public void notifyReactiveUpdate(Value value) {
-		System.out.println("1The reactive value is changed in: " + value.doubleVal());
+		System.out.println("1The reactive value is changed in: " + value.boolVal());
 	}
 }
